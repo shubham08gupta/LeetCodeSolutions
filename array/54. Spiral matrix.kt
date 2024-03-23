f0182d343746b58f37951e51dcbeb1f9b4b05631
shubham08gupta <shubham.bestfriendforu@gmail.com>
2024-03-24 00:31:01 +0100

    Add Leetcode 54
    Spiral matrix

---
+++ array/54. Spiral matrix.kt
@@ -0,0 +1,86 @@
+package com.example.myapplication.leetcode.array
+
+// https://leetcode.com/problems/spiral-matrix/description/
+
+// T.C. O(m * n) m = #rows and n = #columns
+private fun spiralOrder(matrix: Array<IntArray>): List<Int> {
+    val ans = mutableListOf<Int>()
+
+    // init boundary condition for rows
+    var a = 0
+    var b = matrix.size - 1
+
+    // init boundary condition for columns
+    var c = 0
+    var d = matrix.first().size - 1
+
+    // start by printing the row
+    var row = true
+    // print row in reverse or not
+    var rowReverse = false
+    // print column in reverse or not
+    var colReverse = false
+
+    while (a <= b && c <= d) {
+        if (row) {
+            // print row
+            rowReverse = if (rowReverse) {
+                // print(d -> c)
+                for (i in d downTo c) {
+                    ans.add(matrix[b][i])
+                }
+                b--
+                false
+            } else {
+                // print(c -> d)
+                for (i in c..d) {
+                    ans.add(matrix[a][i])
+                }
+                a++
+                true
+            }
+            row = false
+        } else {
+            // print column
+            colReverse = if (colReverse) {
+                // print(b -> a)
+                for (i in b downTo a) {
+                    ans.add(matrix[i][c])
+                }
+                c++
+                false
+            } else {
+                // print(a -> b)
+                for (i in a..b) {
+                    ans.add(matrix[i][d])
+                }
+                d--
+                true
+            }
+            row = true
+        }
+    }
+    return ans
+}
+
+fun main() {
+    val matrix1 = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 6),
+        intArrayOf(7, 8, 9)
+    )
+    println(spiralOrder(matrix1).joinToString())
+
+    val matrix2 = arrayOf(
+        intArrayOf(1, 2, 3, 4),
+        intArrayOf(5, 6, 7, 8),
+        intArrayOf(9, 10, 11, 12)
+    )
+    println(spiralOrder(matrix2).joinToString())
+
+    val matrix3 = arrayOf(
+        intArrayOf(1, 2, 3, 4),
+        intArrayOf(5, 6, 7, 8),
+    )
+    println(spiralOrder(matrix3).joinToString())
+}
