1acc6d0596d6b0f052ec074a7e92f6115d10fe36
shubham08gupta <shubham.bestfriendforu@gmail.com>
2024-04-11 11:04:17 +0200

    Add code to create a sample tree
    Use sample tree for tests

--- tree/TreeNode.kt
+++ tree/TreeNode.kt
@@ -4,3 +4,11 @@ class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
 }
+
+fun createSampleTree(): TreeNode {
+    val root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+
+    return root
+}
